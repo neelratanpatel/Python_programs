384a31d84d61fd0cfee56706f262e331cee5a4ee
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-08-17 20:57:20 +0530

    this is many module files added and upgraded

---
+++ F_06_Modules_and_Packages/package_folder/mathematics.py
@@ -0,0 +1,17 @@
+def addition(a,b):
+    return a+b
+
+def substraction(a,b):
+    if c>d:
+        return c-d
+    else:
+        return d-c
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    if b==0:
+        print("Enther the valid number")
+    else:
+        return a/b
